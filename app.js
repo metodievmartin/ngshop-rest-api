09d3f55d749691fcddf072bc509bd9ad0dbdf984
Martin Metodiev <martin.metodiew@gmail.com>
2021-09-22 23:18:30 +0300

    feat: set-up DB connection via mongoose

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+
+// Set ENV variables from config.env
+dotenv.config({ path: './config.env' });
 
 const app = express();
 
-require('dotenv/config');
+//Middleware
+app.use(express.json());
+app.use(morgan('dev'));
 
-const port = 5000;
+const DB = process.env.DB_CONNECTION;
+const options = {
+  dbName: process.env.DB_NAME
+};
+mongoose.connect(
+  DB,
+  options,
+  (err) => {
+    if (err) {
+      console.log('DB connection error - ', err);
+    } else {
+      console.log('DB connected...');
+    }
+  }
+);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server's running on http://localhost:${port}`);
-})
+});
