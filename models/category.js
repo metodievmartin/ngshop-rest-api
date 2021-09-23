d4def618dc6a4edc6ef1762191407d7b823e5e7c
Martin Metodiev <martin.metodiew@gmail.com>
2021-09-23 16:45:11 +0300

    feat: expand the category schema model
    Add to the category schema model all the necessary fields

--- models/category.js
+++ models/category.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const categorySchema = mongoose.Schema({
-
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+  },
+  colour: {
+    type: String,
+  },
 });
 
 exports.Category = mongoose.model('Category', categorySchema);
