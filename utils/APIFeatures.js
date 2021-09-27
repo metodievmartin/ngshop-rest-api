318792554ab30ae832542579fd20f25b1eda1f6e
Martin Metodiev <martin.metodiew@gmail.com>
2021-09-27 14:28:16 +0300

    feat: create a request queries handling class APIFeatures
    APIFeatures class takes two parameters - mongoose query object &
    request query object, then its methods can be chained to parse the
    request query object and apply filter, sort, limitFields and paginate
    to customize the mongoose query to the db

---
+++ utils/APIFeatures.js
@@ -0,0 +1,79 @@
+class APIFeatures {
+  constructor(query, queryString) {
+    // Mongoose query object
+    this.query = query;
+    // Request query object
+    this.queryString = queryString;
+  }
+
+  // Allows you to narrow down your search by filtering the results
+  filter() {
+    // Copy the query string to not mutate the original object
+    const queryObj = { ...this.queryString };
+
+    // Create an array with the queryObj fields that are to be excluded
+    // (they're not schema fields and are handled by the other methods)
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+
+    // Remove those field from the queryObj
+    excludeFields.forEach(el => delete queryObj[el]);
+
+    // Stringify the queryObj into a queryString so that string operations can be performed on it
+    let queryStr = JSON.stringify(queryObj);
+
+    // Find mongoose operators and add a '$' symbol before each one to make them valid filter operators
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    // Apply the queryObj to the db query filter
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    // Allows method chaining
+    return this;
+  }
+
+  // Allows you to sort the results
+  sort() {
+    if (this.queryString.sort) {
+      // Extract all sorting criteria
+      const sortBy = this.queryString.sort.split(',').join(' ');
+
+      //Apply sorting criteria to the db query
+      this.query = this.query.sort(sortBy);
+    } else {
+      // Default case - the newest will pop up first
+      this.query = this.query.sort('-dateCreated');
+    }
+
+    return this;
+  }
+
+  // Allows you to choose which document fields to be selected
+  limitFields() {
+    if (this.queryString.fields) {
+      // Extracting all fields in one string
+      const fields = this.queryString.fields.split(',').join(' ');
+
+      // Apply only the selected field to the db query
+      this.query = this.query.select(fields);
+    } else {
+      // Default case - the entire doc is being sent without the '__v' field
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  // Allows you to paginate the results by specifying the page number and the results per page
+  paginate() {
+    const page = Number(this.queryString.page) || 1;
+    const limit = Number(this.queryString.limit) || 100;
+    const skip = (page - 1) * limit;
+
+    //Apply pagination criteria to the db query
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
