8fd867f542b077f9d89284114cbd87cef0f447da
Martin Metodiev <martin.metodiew@gmail.com>
2021-10-01 14:39:54 +0300

    feat: create order schema model

--- models/order.js
+++ models/order.js
@@ -1,8 +1,53 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const orderSchema = new Schema({
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
+const orderSchema = new Schema({
+  orderItems: [{
+    type: ObjectId,
+    ref: 'OrderItem',
+    required: [true, 'Please provide order-items']
+  }],
+  shippingAddress1: {
+    type: String,
+    required: [true, 'Please provide a shipping address']
+  },
+  shippingAddress2: {
+    type: String,
+  },
+  city: {
+    type: String,
+    required: [true, 'Please provide a city']
+  },
+  zip: {
+    type: String,
+    required: [true, 'Please provide a zip code']
+  },
+  country: {
+    type: String,
+    required: [true, 'Please provide a country']
+  },
+  phone: {
+    type: String,
+    required: [true, 'Please provide a phone number']
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'Pending',
+  },
+  totalPrice: {
+    type: Number,
+  },
+  user: {
+    type: ObjectId,
+    ref: 'User',
+  },
+  dateOrdered: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 exports.Order = mongoose.model('Order', orderSchema);
